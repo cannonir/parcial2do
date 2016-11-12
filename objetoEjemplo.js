f40c087317bf6de95a797c06052aead427b749ec
unknown <eldel.xperia@gmail.com>
2016-11-12 09:50:13 -0400

    enviar

---
+++ objetoEjemplo.js
@@ -0,0 +1,4 @@
+var dia={nombre:"lunes", hora:"12:00",fecha:"25"}
+console.log("dia: "+dia.nombre);
+console.log("dia: "+dia.hora);
+console.log("dia: "+dia.fecha);
---
+++ tablaSuma.js
@@ -0,0 +1,10 @@
+
+	var x=7;
+	console.log("La suma de numeros 7");
+	for (var i=1; i<=10; i++)
+	{
+		var y=0;		
+		y=x;
+
+		console.log(y+"+"+i+"="+(++x));
+	}
